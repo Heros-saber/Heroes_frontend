8f33e567825e3fdbad2010935215f7553bbc8642
changyunyeong <sally0109277@naver.com>
2024-11-24 20:21:18 +0900

    rename: 선수 디테일 페이지 이름 수정

--- lib/pages/battersearchresultspage.dart
+++ lib/pages/battersearchresultspage.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:heroessaber/pages/moredetailpage.dart';
+import 'package:heroessaber/pages/batterdetailpage.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
